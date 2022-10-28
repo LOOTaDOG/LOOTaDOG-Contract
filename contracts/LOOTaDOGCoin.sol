ff930b942e33c97892301049ac194cabda77deb4
LOOTaDOG <115603913+LOOTaDOG@users.noreply.github.com>
2022-10-28 18:15:18 +0800

    Update LOOTaDOGCoin.sol

--- contracts/LOOTaDOGCoin.sol
+++ contracts/LOOTaDOGCoin.sol
@@ -2,19 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract LOOTaDOGCoin is ERC20, ERC20Burnable, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
+contract LOOTaDOGCoin is ERC20 {
     constructor() ERC20("LOOTaDOGCoin", "LADC") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+        _mint(msg.sender, 600000000000000000);
     }
 
     function decimals() public view virtual override returns (uint8) {
